e387b5780ab42a043bbee2e93ceceeff2c20fbee
Bolesław Tekielski <btekielski@gmail.com>
2014-05-08 11:04:25 +0200

    Initial Commit

---
+++ MongoDB.prefsPane/MongoDB_prefsPane.h
@@ -0,0 +1,15 @@
+//
+//  MongoDB_prefsPane.h
+//  MongoDB.prefsPane
+//
+//  Created by Bolesław Tekielski on 08/05/14.
+//  Copyright (c) 2014 Bolesław Tekielski. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface MongoDB_prefsPane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ MongoDB.prefsPane/MongoDB_prefsPane.m
@@ -0,0 +1,17 @@
+//
+//  MongoDB_prefsPane.m
+//  MongoDB.prefsPane
+//
+//  Created by Bolesław Tekielski on 08/05/14.
+//  Copyright (c) 2014 Bolesław Tekielski. All rights reserved.
+//
+
+#import "MongoDB_prefsPane.h"
+
+@implementation MongoDB_prefsPane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
